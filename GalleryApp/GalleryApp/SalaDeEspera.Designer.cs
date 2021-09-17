f9e1d058883bed101e6f4dd4a148ae0f5db9e253
amaudys <amaudys02@gmail.com>
2021-09-17 15:36:09 -0400

    Carga inicial

---
+++ GalleryApp/GalleryApp/SalaDeEspera.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace GalleryApp
+{
+    partial class SalaDeEspera
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SalaDeEspera));
+            this.lvListaContenido = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lvListaContenido
+            // 
+            this.lvListaContenido.AllowDrop = true;
+            this.lvListaContenido.Location = new System.Drawing.Point(12, 43);
+            this.lvListaContenido.Name = "lvListaContenido";
+            this.lvListaContenido.ShowItemToolTips = true;
+            this.lvListaContenido.Size = new System.Drawing.Size(745, 514);
+            this.lvListaContenido.TabIndex = 0;
+            this.lvListaContenido.UseCompatibleStateImageBehavior = false;
+            this.lvListaContenido.View = System.Windows.Forms.View.List;
+            this.lvListaContenido.DragDrop += new System.Windows.Forms.DragEventHandler(this.lvListaContenido_DragDrop);
+            this.lvListaContenido.DragEnter += new System.Windows.Forms.DragEventHandler(this.lvListaContenido_DragEnter);
+            // 
+            // SalaDeEspera
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 569);
+            this.Controls.Add(this.lvListaContenido);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SalaDeEspera";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sala de Espera";
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.SalaDeEspera_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvListaContenido;
+    }
+}
+
---
+++ GalleryApp/GalleryApp/SalaDeEspera.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GalleryApp
+{
+    public partial class SalaDeEspera : Form
+    {
+        public SalaDeEspera()
+        {
+            InitializeComponent();
+        }
+
+        private void lvListaContenido_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void SalaDeEspera_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+            {
+                String[] strGetFormats = e.Data.GetFormats();
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void lvListaContenido_DragDrop(object sender, DragEventArgs e)
+        {
+            List<string> filepaths = new List<string>();
+            var listaContenido = sender as ListView;
+
+            foreach (var s in (string[])e.Data.GetData(DataFormats.FileDrop, false))
+            {
+                if (Directory.Exists(s))
+                {
+                    filepaths.AddRange(Directory.GetFiles(s));
+                    //listaContenido.Items.Add(Directory.GetFiles(s));
+                }
+                else
+                {
+                    filepaths.Add(s);
+                    listaContenido.Items.Add(s);
+                }
+            }
+        }
+    }
+}
